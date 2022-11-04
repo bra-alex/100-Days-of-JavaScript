8ba4e8f7ed091cd53ca0fda8fca2bf0928372f45
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-11-04 14:11:31 +0000

    Day 31, add cluster

--- node/performance-example/server.js
+++ node/performance-example/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cluster = require('cluster')
 
 const PORT = process.env.PORT || 3000
 
@@ -11,12 +12,19 @@ function delay(duration){
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance example')
+    res.send(`Performance example: ${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
     delay(9000)
-    res.send('hi')
+    res.send(`hi: ${process.pid}`)
 })
 
-app.listen(PORT, () => console.log('Listening on port', PORT))
+if (cluster.isMaster){
+    console.log('Master has been started')
+    cluster.fork()
+    cluster.fork()
+} else {
+    console.log('Worker has been started')
+    app.listen(PORT, () => console.log('Listening on port', PORT))
+}
