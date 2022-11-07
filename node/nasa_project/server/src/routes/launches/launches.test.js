dc126ee1ec69d2f79812274a52655659c29b0572
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-11-07 21:08:06 +0000

    Day 34, update test

--- node/nasa_project/server/src/routes/launches/launches.test.js
+++ node/nasa_project/server/src/routes/launches/launches.test.js
@@ -1,72 +1,85 @@
 const request = require('supertest')
 const app = require('../../app')
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo')
 
-describe('Test GET /launches', () =>{
-    test('Should respond with 200', async () => { 
-        await request(app)
-            .get('/launches')
-            .expect('Content-Type', /json/)
-            .expect(200)
-    })
-})
+describe('Launches API', () => {
+    jest.setTimeout(15000);
 
-describe('Test POST /launches', () => {
-    const launchData = {
-        mission: 'Mission to the west',
-        rocket: 'Explorer IS1',
-        target: 'Kepler-442 b',
-        launchDate: "January 21, 2099"
-    }
-
-    const launchDataWithInvalidDate = {
-        mission: 'Mission to the west',
-        rocket: 'Explorer IS1',
-        target: 'Kepler-442 b',
-        launchDate: "hi"
-    }
-
-    const launchDataWithoutDate = {
-        mission: 'Mission to the west',
-        rocket: 'Explorer IS1',
-        target: 'Kepler-442 b'
-    }
-
-    test('Should respond with 201 created', async () => {
-        const response = await request(app)
-            .post('/launches')
-            .send(launchData)
-            .expect('Content-Type', /json/)
-            .expect(201)
-
-        const requestDate = new Date(launchData.launchDate).valueOf()
-        const responseDate = new Date(response.body.launchDate).valueOf()
-
-        expect(requestDate).toBe(responseDate)
-        expect(response.body).toMatchObject(launchDataWithoutDate) 
-
+    beforeAll(async () => {
+        await mongoConnect()
     })
 
-    test('Missing mission parameter', async () => {
-        const response = await request(app)
-            .post('/launches')
-            .send(launchDataWithoutDate)
-            .expect('Content-Type', /json/)
-            .expect(400)
+    afterAll(async () => {
+        await mongoDisconnect()
+    })
 
-        expect(response.body).toStrictEqual({
-            error: "Missing mission parameter"
+    describe('Test GET /launches', () =>{
+        test('Should respond with 200', async () => { 
+            await request(app)
+                .get('/launches')
+                .expect('Content-Type', /json/)
+                .expect(200)
         })
     })
 
-    test('"Wrong date input"', async () => {
-        const response = await request(app)
-            .post('/launches')
-            .send(launchDataWithInvalidDate)
-            .expect('Content-Type', /json/)
-            .expect(400)
+    describe('Test POST /launches', () => {
+        const launchData = {
+            mission: 'Mission to the west',
+            rocket: 'Explorer IS1',
+            target: 'Kepler-442 b',
+            launchDate: "January 21, 2099"
+        }
 
-        expect(response.body).toStrictEqual({
-            error: "Wrong date input"
+        const launchDataWithInvalidDate = {
+            mission: 'Mission to the west',
+            rocket: 'Explorer IS1',
+            target: 'Kepler-442 b',
+            launchDate: "hi"
+        }
+
+        const launchDataWithoutDate = {
+            mission: 'Mission to the west',
+            rocket: 'Explorer IS1',
+            target: 'Kepler-442 b'
+        }
+
+        test('Should respond with 201 created', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchData)
+                .expect('Content-Type', /json/)
+                .expect(201)
+
+            const requestDate = new Date(launchData.launchDate).valueOf()
+            const responseDate = new Date(response.body.launchDate).valueOf()
+
+            expect(requestDate).toBe(responseDate)
+            expect(response.body).toMatchObject(launchDataWithoutDate) 
+
+        })
+
+        test('Missing mission parameter', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchDataWithoutDate)
+                .expect('Content-Type', /json/)
+                .expect(400)
+
+            expect(response.body).toStrictEqual({
+                error: "Missing mission parameter"
+            })
+        })
+
+        test('"Wrong date input"', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchDataWithInvalidDate)
+                .expect('Content-Type', /json/)
+                .expect(400)
+
+            expect(response.body).toStrictEqual({
+                error: "Wrong date input"
+            })
         })
     })
 })
