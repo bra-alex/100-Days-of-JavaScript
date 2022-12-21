a2be40945a76a904136aca47122d6193e882e114
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-21 18:43:37 +0000

    Day 53, connect to database

--- node/revision/restAPI/server/app.js
+++ node/revision/restAPI/server/app.js
@@ -1,6 +1,8 @@
 const cors = require('cors')
 const express = require('express')
 
+const { mongoConnect } = require('./util/database')
+
 const feedRouter = require('./routes/feed.route')
 
 const PORT = 8080
@@ -10,19 +12,26 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://cdpn.io',
-    methods: ['GET', 'POST'],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://cdpn.io')
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
-//     res.setHeader('Access-Control-Allow-Allow-Headers', 'Content-Type, Authorization')
+/*
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://cdpn.io')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Allow-Headers', 'Content-Type, Authorization')
 
-//     next()
-// })
+    next()
+})
+*/
 
-app.use(feedRouter)
+app.use('/feed', feedRouter)
 
-app.listen(PORT, () => console.log('Listening on port: ', PORT))
+async function startServer() {
+    await mongoConnect()
+    app.listen(PORT, () => console.log('Listening on port: ', PORT))
+}
+
+startServer()
