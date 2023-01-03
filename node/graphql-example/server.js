6d778a78e6ebf60a728d26e642f92c4bbfabe0ee
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-01-03 07:34:44 +0000

    Day 61, add Apollo Server

--- node/graphql-example/server.js
+++ node/graphql-example/server.js
@@ -1,29 +1,30 @@
 const path = require('path')
 const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
 const { loadFilesSync } = require('@graphql-tools/load-files')
-const { createHandler } = require('graphql-http/lib/use/express')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
+
+const PORT = 3000
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'))
 
-const schema = makeExecutableSchema({
-    typeDefs: typesArray,
-    resolvers: resolversArray
-})
+async function startApolloServer(){
+    const app = express()
+    
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray
+    })
 
-const root = {
-    products: require('./products/products.model'),
-    orders: require('./orders/orders.model'),
+    const server = new ApolloServer({
+        schema
+    })
+
+    await server.start()
+    server.applyMiddleware({app, path: '/graphql'})
+
+    app.listen(PORT, () => console.log('Running GraphQL server on port: ', PORT))
 }
 
-const PORT = 3000
-
-const app = express()
-
-app.use('/graphql', createHandler({
-    schema: schema,
-    rootValue: root
-}))
-
-app.listen(PORT, () => console.log('Running GraphQL server on port: ', PORT))
+startApolloServer()
