0bb369035bd76fa23da1f91d9c076597feb99ed6
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-07 09:54:33 +0000

    update valid parenthesis

--- dsa/valid-parenthesis.js
+++ dsa/valid-parenthesis.js
@@ -4,16 +4,19 @@
  */
 var isValid = function (s) {
     const stack = []
+    let count = 0
     for (let i = 0; i < s.length; i++) {
         if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
             stack.push(s[i])
+            count++
         } else {
             if (stack.length === 0) return false
-            let top = stack[stack.length - 1]
+            let top = stack[count - 1]
             if ((top === '(' && s[i] === ')')
                 || (top === '{' && s[i] === '}')
                 || (top === '[' && s[i] === ']')) {
                 stack.pop()
+                count--
             } else {
                 return false
             }
