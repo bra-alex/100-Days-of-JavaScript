21a085598cfbb4c3fe9634e3a795299ca562e634
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-07 09:08:12 +0000

    update reverse integer

--- dsa/reverse-integer.js
+++ dsa/reverse-integer.js
@@ -4,24 +4,16 @@
  */
 var reverse = function (x) {
     let r = 0
+    let num = x < 0 ? x * -1 : x
     const INT_MAX = Math.pow(2, 31) - 1
     const INT_MIN = Math.pow(-2, 31)
 
-    if (x < 0) {
-        x *= -1
-        while (x > 0) {
-            let mod = x % 10
-            r = r * 10 + mod
-            x = Math.floor(x / 10)
-        }
-        r *= -1
-    } else {
-        while (x > 0) {
-            let mod = x % 10
-            r = r * 10 + mod
-            x = Math.floor(x / 10)
-        }
+    while (num > 0) {
+        let mod = num % 10
+        r = r * 10 + mod
+        num = Math.floor(num / 10)
     }
+    r = x < 0 ? r * -1 : r
 
     return r < INT_MIN || r > INT_MAX ? 0 : r
 };
