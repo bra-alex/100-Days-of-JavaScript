a2e9429af7d86893549be50257b8462801c48cc4
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-07 09:04:26 +0000

    add valid paranthesis

---
+++ dsa/valid-parenthesis.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = []
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i])
+        } else {
+            if (stack.length === 0) return false
+            let top = stack[stack.length - 1]
+            if ((top === '(' && s[i] === ')')
+                || (top === '{' && s[i] === '}')
+                || (top === '[' && s[i] === ']')) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0 ? true : false
+};
+console.log(isValid('((()))[][][]'));
